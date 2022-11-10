5f536b9c29c7f79cf36787c2900b6b5c1e79aeb5
Naveen Kumar Vadlamudi <39674418+VadlamudiNaveen@users.noreply.github.com>
2022-11-09 20:42:35 -0700

    09112022

---
+++ SQL/596. Classes More Than 5 Students.sql
@@ -0,0 +1,72 @@
+596. Classes More Than 5 Students
+Easy
+
+/*
+Create table If Not Exists Courses (student varchar(255), class varchar(255))
+Truncate table Courses
+insert into Courses (student, class) values ('A', 'Math')
+insert into Courses (student, class) values ('B', 'English')
+insert into Courses (student, class) values ('C', 'Math')
+insert into Courses (student, class) values ('D', 'Biology')
+insert into Courses (student, class) values ('E', 'Math')
+insert into Courses (student, class) values ('F', 'Computer')
+insert into Courses (student, class) values ('G', 'Math')
+insert into Courses (student, class) values ('H', 'Math')
+insert into Courses (student, class) values ('I', 'Math')
+*/
+
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write an SQL query to report all the classes that have at least five students.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+
+--SOLUTION
+# Code Author Naveen Kumar Vadlamudi
+SELECT class
+FROM COURSES
+GROUP BY CLASS
+HAVING COUNT(*) >= 5
