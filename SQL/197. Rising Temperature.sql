803da56385abe27a005ce8e20cbe420e92540d11
Naveen Kumar Vadlamudi <39674418+VadlamudiNaveen@users.noreply.github.com>
2022-11-29 15:22:56 -0700

    29112022
    Added Second Solution to Rising Temperature problem.

--- SQL/197. Rising Temperature.sql
+++ SQL/197. Rising Temperature.sql
@@ -2,14 +2,16 @@
 
 link: https://leetcode.com/problems/rising-temperature/
 
-/*
-Create table If Not Exists Weather (id int, recordDate date, temperature int)
-Truncate table Weather
-insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
-insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
-insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
-insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
-*/
+-- TABLE SCHEMA
+Create table If Not Exists Weather (id int, recordDate date, temperature int);
+Truncate table Weather;
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25');
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
+
+-- PROBLEM STATEMENT
+
 Table: Weather
 
 +---------------+---------+
@@ -54,7 +56,7 @@ Explanation:
 In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 
---SOLUTION 
+-- SOLUTION 
 
 select w1.id
 from 
@@ -67,3 +69,24 @@ are trying to match the d2 with d1 by adding +1 to it...
 */
 to_days(w1.recordDate) = to_days(w2.recordDate) + 1
 and w1.temperature > w2.temperature
+
+-- SECOND SOLUTION USING LAG + CTE + CASE 
+-- CODE AUTHOR NAVEEN KUMAR VADLAMUDI
+
+WITH CTE AS 
+(
+SELECT *
+FROM WEATHER 
+ORDER BY RECORDDATE ASC
+)
+
+SELECT ID
+FROM 
+(
+    SELECT 
+    CASE 
+        WHEN TEMPERATURE > LAG(TEMPERATURE) OVER() AND DATEDIFF(RECORDDATE,LAG(RECORDDATE)OVER()) = 1 THEN ID
+    END AS ID
+    FROM CTE
+) AS A
+WHERE A.ID IS NOT NULL
