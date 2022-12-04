77c2f78393471cd8f5028698a62ae1f19a38b8b8
Naveen Kumar Vadlamudi <39674418+VadlamudiNaveen@users.noreply.github.com>
2022-12-03 21:15:29 -0700

    03122022
    Added Extra Layers

--- SQL/596. Classes More Than 5 Students.sql
+++ SQL/596. Classes More Than 5 Students.sql
@@ -1,20 +1,20 @@
 596. Classes More Than 5 Students
 Easy
 
-/*
-Create table If Not Exists Courses (student varchar(255), class varchar(255))
-Truncate table Courses
-insert into Courses (student, class) values ('A', 'Math')
-insert into Courses (student, class) values ('B', 'English')
-insert into Courses (student, class) values ('C', 'Math')
-insert into Courses (student, class) values ('D', 'Biology')
-insert into Courses (student, class) values ('E', 'Math')
-insert into Courses (student, class) values ('F', 'Computer')
-insert into Courses (student, class) values ('G', 'Math')
-insert into Courses (student, class) values ('H', 'Math')
-insert into Courses (student, class) values ('I', 'Math')
-*/
+-- Data Model Schema 
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
 
+-- Question 
 Table: Courses
 
 +-------------+---------+
@@ -64,8 +64,9 @@ Explanation:
 - Biology has 1 student, so we do not include it.
 - Computer has 1 student, so we do not include it.
 
---SOLUTION
-# Code Author Naveen Kumar Vadlamudi
+-- SOLUTION
+-- Code Author Naveen Kumar Vadlamudi
+
 SELECT class
 FROM COURSES
 GROUP BY CLASS
